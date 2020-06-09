e7e34571c7c1b96dac6cd658009d30b732b3df2f
sohum7777 <57050073+sohum7777@users.noreply.github.com>
2020-06-09 17:04:01 -0400

    Add files via upload

---
+++ src/config/config.ts
@@ -0,0 +1,19 @@
+export const config = {
+  "dev": {
+    "username": "sohum7",
+    "password": "",
+    "database": "TEST_database_sohum",
+    "host": "test-database.cxwp8tjkffmb.us-east-2.rds.amazonaws.com",
+    "dialect": "postgres",
+    "aws_region": "us-east-2",
+    "aws_profile": "default",
+    "aws_media_bucket": "test-patel-restapi-dev"
+  },
+  "prod": {
+    "username": "",
+    "password": "",
+    "database": "udagram_prod",
+    "host": "",
+    "dialect": "postgres"
+  }
+}
